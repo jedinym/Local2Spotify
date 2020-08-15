f64f6317d6c83a08daac36ee76730c751c2dca93
Martin Jediny <jedinym@gmail.com>
2020-08-15 16:05:56 +0200

    Implented basic Spotify API functions

--- Program.cs
+++ Program.cs
@@ -10,62 +10,45 @@ namespace TagTest
     {
         static void Main(string[] args)
         {
-            string[] mp3FilePaths = Directory.GetFiles(@"D:\Music", "*.mp3", SearchOption.AllDirectories);
-            string[] flacFilePaths = Directory.GetFiles(@"D:\Music", "*.flac", SearchOption.AllDirectories);
+            GetAlbumListFromPath(@"D:\Music");
+        }
 
-            List<string> filePaths = new List<string>();
-            
-            foreach (string path in mp3FilePaths)
-            {
-                filePaths.Add(path);
-            }
-
-            foreach (string path in flacFilePaths)
-            {
-                filePaths.Add(path);
-            }
-
+        private static List<string> GetAlbumListFromPath(string _path)
+        {
+            string[] mp3FilePaths = Directory.GetFiles(_path, "*.mp3", SearchOption.AllDirectories);
+            string[] flacFilePaths = Directory.GetFiles(_path, "*.flac", SearchOption.AllDirectories);
 
             HashSet<string> albumNames = new HashSet<string>();
 
-            TagLib.File f = TagLib.File.Create(@"D:\Music\Air\Moon Safari (Album)\02 Sexy Boy.mp3");
+            TagLib.File f;
 
-            Stopwatch sw = new Stopwatch();
-
-            foreach (string path in filePaths)
+            for (int i = 0; i < mp3FilePaths.Length; ++i)
             {
-                //sw.Start();
                 try
                 {
-                    f = TagLib.File.Create(path);
+                    f = TagLib.File.Create(mp3FilePaths[i]);
+                    albumNames.Add(f.Tag.Album);
                 }
-                catch (TagLib.CorruptFileException)
+                catch (CorruptFileException)
                 {
-                    Console.WriteLine("Path " + path + " Has no tags");
+                    Console.WriteLine("File " + mp3FilePaths[i] + " Has no tags");
                 }
-                //sw.Stop();
-
-                //Console.WriteLine(sw.ElapsedMilliseconds);
-                //sw.Reset();
-
-
-                albumNames.Add(f.Tag.Album);
-
             }
 
-            List<string> albumNameList = new List<string>(albumNames);
-            albumNameList.Sort();
-
-            foreach (string name in albumNameList)
+            for (int i = 0; i < flacFilePaths.Length; ++i)
             {
-                Console.WriteLine(name);
+                try
+                {
+                    f = TagLib.File.Create(flacFilePaths[i]);
+                    albumNames.Add(f.Tag.Album);
+                }
+                catch (CorruptFileException)
+                {
+                    Console.WriteLine("File " + flacFilePaths[i] + " Has no tags");
+                }
             }
 
-            Console.WriteLine(albumNameList.Count);
-
-            //TagLib.File f = TagLib.File.Create(@"D:\Music\Amon Tobin\Out From Out Where (Album)\01 Back From Space.mp3");
-            //Console.WriteLine(f.Tag.Title);
-            
+            return new List<string>(albumNames);
         }
     }
 }
